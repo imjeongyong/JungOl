b0a9a1105870e001732419b38dc5f954a8275512
Administrator <Administrator@User-2022RPLKEB>
2022-11-02 13:03:07 +0900

    모범답안 추가

--- JungOl501/src/Output506.java
+++ JungOl501/src/Output506.java
@@ -9,3 +9,22 @@ public class Output506 {
 		System.out.printf("%2.6f", d);
 	}
 }
+
+
+
+/* 모범답안
+public class Output506 {
+	public static void main(String[] args) {
+		String str1 = "My height";
+		int height = 170;
+		String str2 = "My height";
+		double d = 68.6;
+
+		System.out.println(str1);
+		System.out.println(height);
+		System.out.println(str2);
+		System.out.printf("%.6f", d);
+	}
+}
+
+*/
